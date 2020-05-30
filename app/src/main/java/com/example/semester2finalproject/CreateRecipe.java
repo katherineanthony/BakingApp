3a5f2ab35bcbdb5f9b35951b3c3faf333fd757e1
katherineanthony <katherinesanthony@gmail.com>
2020-05-30 14:49:19 -0700

    create new recipe

--- app/src/main/java/com/example/semester2finalproject/CreateRecipe.java
+++ app/src/main/java/com/example/semester2finalproject/CreateRecipe.java
@@ -7,7 +7,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.backendless.Backendless;
+import com.backendless.async.callback.AsyncCallback;
+import com.backendless.exceptions.BackendlessFault;
 
 
 /**
@@ -27,6 +33,7 @@ public class CreateRecipe extends Fragment {
     private EditText directions;
     private EditText ingredients;
     private EditText recipeName;
+    private Button saveButton;
     private Recipe recipe;
 
     public CreateRecipe() {
@@ -66,14 +73,31 @@ public class CreateRecipe extends Fragment {
         // Inflate the layout for this fragment
         View rootview =  inflater.inflate(R.layout.fragment_create_recipe, container, false);
         wireWidgets(rootview);
-        setOnClickListeners();
+        setListeners();
         return inflater.inflate(R.layout.fragment_create_recipe, container, false);
     }
 
-    private void setOnClickListeners() {
+    private void setListeners() {
         recipe.setDirections(directions.getText().toString());
         recipe.setRecipeName(recipeName.getText().toString());
         recipe.setIngredients(ingredients.getText().toString());
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Backendless.Data.of(Recipe.class).save(recipe, new AsyncCallback<Recipe>() {
+                    @Override
+                    public void handleResponse(Recipe response) {
+                        Toast.makeText(CreateRecipe.this.getContext(), "it worked", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void handleFault(BackendlessFault fault) {
+                        Toast.makeText(CreateRecipe.this.getContext(), "it did not work", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+            }
+        });
     }
 
     private void wireWidgets(View rootview) {
@@ -81,6 +105,7 @@ public class CreateRecipe extends Fragment {
         ingredients=rootview.findViewById(R.id.editText_createRecipe_ingredients);
         recipeName=rootview.findViewById(R.id.editText_createRecipe_recipeName);
         directions=rootview.findViewById(R.id.editText_createRecipe_directions);
+        saveButton=rootview.findViewById(R.id.button_createRecipe_save);
 
     }
 
