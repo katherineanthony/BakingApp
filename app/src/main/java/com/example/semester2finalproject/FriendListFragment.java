9134830670b1e1c2decb90c4b63a7019a09e792d
katherineanthony <katherinesanthony@gmail.com>
2020-05-30 15:18:06 -0700

    friendlist for andrew to do

--- app/src/main/java/com/example/semester2finalproject/FriendListFragment.java
+++ app/src/main/java/com/example/semester2finalproject/FriendListFragment.java
@@ -7,6 +7,16 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.backendless.Backendless;
+import com.backendless.BackendlessUser;
+import com.backendless.async.callback.AsyncCallback;
+import com.backendless.exceptions.BackendlessFault;
+
+import java.util.List;
 
 
 /**
@@ -14,6 +24,7 @@ import android.view.ViewGroup;
  * Use the {@link FriendListFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
+
 public class FriendListFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -23,6 +34,10 @@ public class FriendListFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private ListView friendListView;
+    private TextView friendName;
+    private TextView friendRecipe;
+    private FriendAdapter friendAdapter;
 
     public FriendListFragment() {
         // Required empty public constructor
@@ -59,6 +74,57 @@ public class FriendListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        View rootview =  inflater.inflate(R.layout.fragment_friends, container, false);
+        wireWidgets(rootview);
+        setListeners();
+
+        Backendless.Data.of(BackendlessUser.class).find(new AsyncCallback<List<BackendlessUser>>() {
+            @Override
+            public void handleResponse(List<BackendlessUser> response) {
+
+            }
+
+            @Override
+            public void handleFault(BackendlessFault fault) {
+
+            }
+        });
         return inflater.inflate(R.layout.fragment_friends, container, false);
+    }
+
+    private void wireWidgets(View rootview) {
+        friendListView=rootview.findViewById(R.id.friends_listView_friendList);
+    }
+
+    private void setListeners() {
+
+    }
+
+    private class FriendAdapter extends ArrayAdapter {
+        private List<Recipe> friendList;
+        private int position;
+
+        public FriendAdapter(List<Recipe> friendsList) {
+            super(FriendListFragment.this.getContext(), -1, friendsList);
+            this.friendList = friendsList;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            this.position = position;
+            LayoutInflater inflater = getLayoutInflater();
+            if (convertView == null) {
+                convertView = inflater.inflate(R.layout.item_friend, parent, false);
+            }
+
+            friendName = convertView.findViewById(R.id.textView_friendItem_friendName);
+            friendRecipe = convertView.findViewById(R.id.textView_friendItem_friendRecipe);
+
+
+            friendName.setText(friendList.get(position).getRecipeName());
+            friendRecipe.setText(friendList.get(position).getRecipeName());
+
+            return convertView;
+        }
     }
 }
