45cd382ced0092c6d20f775d32f915a081a06bc7
Andrew Parkinson <parkyandrew1@gmail.com>
2020-05-30 00:19:06 -0700

    I just realised I had the same commit message the last lots of times

--- app/src/main/java/com/example/semester2finalproject/AddFriend.java
+++ app/src/main/java/com/example/semester2finalproject/AddFriend.java
@@ -4,21 +4,26 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.backendless.Backendless;
+import com.backendless.BackendlessUser;
+import com.backendless.async.callback.AsyncCallback;
+import com.backendless.exceptions.BackendlessFault;
+
+import java.util.List;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AddFriend#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class AddFriend extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -28,37 +33,29 @@ public class AddFriend extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AddFriend.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AddFriend newInstance(String param1, String param2) {
-        AddFriend fragment = new AddFriend();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_friend, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_add_friend, container, false);
+        String username = "";
+        Backendless.Persistence.of(BackendlessUser.class).addRelation(Backendless.UserService.CurrentUser(),
+                "friend", "username = " + username, new AsyncCallback<Integer>() {
+                    @Override
+                    public void handleResponse(Integer response) {
+
+                    }
+
+                    @Override
+                    public void handleFault(BackendlessFault fault) {
+
+                    }
+                });
+        return rootView;
     }
 }
